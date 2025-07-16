a8b591eae25291862a67ecec6118f5ffdde8222a
Cyb3rFox1337 <sebastianlukas.stecher@gmail.com>
2025-07-16 22:24:18 +0200

    addedClasses

---
+++ LMBES/Models/Foodgroup.cs
@@ -0,0 +1,15 @@
+﻿namespace LMBES.Models
+{
+    public class Foodgroup
+    {
+        protected int ID { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        public Foodgroup(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ LMBES/Models/Groceries.cs
@@ -0,0 +1,16 @@
+﻿namespace LMBES.Models
+{
+    public class Groceries : Food
+    {
+        public DateTime ExpirationDate { get; private set; }
+        public int Amount { get; private set; }
+        public Groceries(int id, string name, Foodgroup foodgroup, DateTime expirationDate, int amountPerPackage, UOMs uom, string description, byte[]? image) : base(id, name, foodgroup, amountPerPackage, uom, description, image)
+        {
+            ExpirationDate = expirationDate;
+        }
+        public void ChangeExpirationDate (DateTime expirationDate)
+        {
+            ExpirationDate = expirationDate;
+        }
+    }
+}
